0fa256e5cac30120d225c19cc1beeab90ee7e0b9
matweeew <grmtveeen@yandex.ru>
2024-06-27 15:26:31 +0300

    Add CMakeFiles

---
+++ Files to compile/CMakeFiles/atom_profiles.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atom_profiles.dir/kernel.cu.o"
+  "CMakeFiles/atom_profiles.dir/kernel.cu.o.d"
+  "atom_profiles"
+  "atom_profiles.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/atom_profiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
